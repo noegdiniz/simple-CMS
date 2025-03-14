a2928c045e9835b223a65cfbbed6d862b45e5c3f
nettosz <noegomes@TI-NTB-NOE.amcel.stn>
2025-03-14 08:26:23 -0300

    titulo padrao adicionad

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -14,7 +14,8 @@ def index():
     fotos = Galerie.query.order_by(desc(Galerie.created_at)).limit(6).all()
     noticias = News.query.order_by(desc(News.created_at)).limit(6).all()
 
-    return render_template('index.html', home=home, fotos=fotos, noticias=noticias)
+    titulo_padrao = Sobre.query.all()[1]
+    return render_template('index.html', home=home, fotos=fotos, noticias=noticias, titulo_padrao=titulo_padrao)
 
 ############################################################
 
@@ -22,8 +23,10 @@ def index():
 @main_bps.route('/galerie/content/<foto_id>')
 def galerie_content(foto_id):
     galerie_types = Gallerie_types.query.all()
-    galeria = Galerie.query.filter(Galerie.id == foto_id).first()
-    return render_template('galerie_content.html', galeria=galeria, galerie_types=galerie_types)
+    galeria = Galerie.query.filter(Galerie.id == foto_id).first(),
+
+    titulo_padrao = Sobre.query.all()[1]
+    return render_template('galerie_content.html', galeria=galeria, galerie_types=galerie_types, titulo_padrao=titulo_padrao)
 
 #Galeria: Retorna Page
 @main_bps.route('/galerie/page/<type_id>')
@@ -36,9 +39,8 @@ def galerie_page(type_id=None):
     else:
         galerie_db = Galerie.query.all()
 
-    home = Sobre.query.all()[0]
-
-    return render_template('galerie_page.html', home=home, type_id=type_id, galerie_types=galerie_types, galerie_db=galerie_db)
+    titulo_padrao = Sobre.query.all()[1]
+    return render_template('galerie_page.html', type_id=type_id, galerie_types=galerie_types, galerie_db=galerie_db, titulo_padrao=titulo_padrao)
 
 ##############################################################
 
@@ -52,9 +54,8 @@ def noticias_page(type_id = None):
     else:
         noticias_db = News.query.all()
 
-    home = Sobre.query.all()[0]
-
-    return render_template('news_page.html', home=home, type_id=type_id, noticias_db=noticias_db, noticias_types=noticias_types)
+    titulo_padrao = Sobre.query.all()[1]
+    return render_template('news_page.html', type_id=type_id, noticias_db=noticias_db, noticias_types=noticias_types, titulo_padrao=titulo_padrao)
 
 
 #Noticias: Retorna conteudo
@@ -63,16 +64,24 @@ def noticias_content(news_id):
     noticias_types = News_types.query.all()
     noticia = News.query.filter(News.id == news_id).first()
 
-    return render_template('news_content.html', noticia=noticia, noticias_types=noticias_types)
+
+    titulo_padrao = Sobre.query.all()[1]
+    return render_template('news_content.html', noticia=noticia, noticias_types=noticias_types, titulo_padrao=titulo_padrao)
 
 #################################################################
 
 #Sobre: Retorna Hub
+@main_bps.route('/sobre/page/<id>')
 @main_bps.route('/sobre/page')
-def sobre_page():
+def sobre_page(id=None):
     sobre_db = Sobre.query.all()[1:]
-    home = Sobre.query.all()[0]
-    return render_template('sobre_page.html', sobre_db=sobre_db, home=home)
+
+    if id:
+        sobre = Sobre.query.filter(Sobre.id == id).first()
+    else:
+        sobre = None
+
+    return render_template('sobre_page.html', sobre_db=sobre_db, sobre=sobre)
 
 ################################################################
 def configure(app):
